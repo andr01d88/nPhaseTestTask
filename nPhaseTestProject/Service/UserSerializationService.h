cceba717c886525a42fa70952c5385b3352094bf
Andrew Koryagin <andrewko@gmx.com>
2024-10-25 11:19:16 +0300

    Implement User Serialization service

--- nPhaseTestProject/Service/UserSerializationService.h
+++ nPhaseTestProject/Service/UserSerializationService.h
@@ -8,5 +8,13 @@
 #ifndef UserSerializationService_h
 #define UserSerializationService_h
 
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+@interface UserSerializationService : NSObject
+- (void)serializeUser:(User *_Nullable)user completion:(void (^_Nullable)(NSString * _Nullable jsonString, NSError * _Nullable error))completion;
+- (void)deserializeUserFromJSON:(NSString *_Nullable)jsonString completion:(void (^_Nullable)(User * _Nullable user, NSError * _Nullable error))completion;
+
+@end
 
 #endif /* UserSerializationService_h */
--- nPhaseTestProject/Service/UserSerializationService.m
+++ nPhaseTestProject/Service/UserSerializationService.m
@@ -5,4 +5,50 @@
 //  Created by Andrew Koryagin on 25.10.24.
 //
 
-#import <Foundation/Foundation.h>
+#import "UserSerializationService.h"
+#import <Mantle/Mantle.h>
+
+@implementation UserSerializationService
+
+- (void)serializeUser:(User *)user completion:(void (^)(NSString * _Nullable jsonString, NSError * _Nullable error))completion {
+    NSError *error = nil;
+    NSDictionary *userDict = [MTLJSONAdapter JSONDictionaryFromModel:user error:&error];
+    if (error) {
+        if (completion) {
+            completion(nil, error);
+        }
+        return;
+    }
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:userDict options:NSJSONWritingPrettyPrinted error:&error];
+    if (error) {
+        if (completion) {
+            completion(nil, error);
+        }
+        return;
+    }
+    
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    if (completion) {
+        completion(jsonString, nil);
+    }
+}
+
+- (void)deserializeUserFromJSON:(NSString *)jsonString completion:(void (^)(User * _Nullable user, NSError * _Nullable error))completion {
+    NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    if (error) {
+        if (completion) {
+            completion(nil, error);
+        }
+        return;
+    }
+    
+    User *user = [MTLJSONAdapter modelOfClass:User.class fromJSONDictionary:jsonDict error:&error];
+    if (completion) {
+        completion(user, error);
+    }
+}
+
+@end
